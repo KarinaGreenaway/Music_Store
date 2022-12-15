999e2db9513d81864f38d584ad03b7ce0b550523
KarinaGreenaway <117394882+KarinaGreenaway@users.noreply.github.com>
2022-12-15 12:13:17 +0000

    CSS Finishing Touches

--- signin.php
+++ signin.php
@@ -59,9 +59,9 @@ include_once 'header.php';
           </form>
         </div>
       </div>
-      <div class="col-sm-6 px-0 d-none d-sm-block">
+      <div class="col-sm-6 px-0 d-none d-sm-block mh-100 min-vh-100">
         <img src="Images/instrument-1.jpg"
-          alt="Login image" class="w-100 h-100"  style="object-fit: cover; object-position: left;">
+          alt="Login image" class="w-100 h-100 min-vh-100"  style="object-fit: cover; object-position: left;">
       </div>
     </div>
   </div>
